89b0c642289221b0be5763f3c1c54394ee8315bb
tyseawood <tyseawood@protonmail.com>
2023-06-14 21:14:32 -0700

    feat:Add error detection for cardlist component

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 
 class App extends Component {
   constructor(props) {
@@ -35,7 +36,9 @@ class App extends Component {
         <h1 className={'f1'}>RobotFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
-          <CardList robots={filteredRobots} />
+          <ErrorBoundry>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundry>
         </Scroll>
       </div>
     );
